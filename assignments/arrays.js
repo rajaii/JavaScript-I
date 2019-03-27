12abfb6c8f1d148f935f5e09bb942ddc5afcdb12
rajaii_a <rajaii_a@yahoo.com>
2019-03-27 16:47:59 -0400

    all done with stretch

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -83,20 +83,31 @@ console.log(carModelsSorted);
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-for (let i = 0; i < inventory.length; i++) {
+ /*Regular*/ for (let i = 0; i < inventory.length; i++) {
     carYears.push(inventory[i].car_year);
 }
+//Stretch:
+carYears.push(inventory.map(item => item.car_year));
+
 console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-for (let i = 0; i < inventory.length; i++) {
+//Way one:
+ for (let i = 0; i < inventory.length; i++) {
     if (inventory[i].car_year < 2000) {
         oldCars.push(inventory[i].car_year);
     }
 }
-console.log(oldCars);
+// Stretch way:
+const year = inventory.filter(i => i.car_year < 2000);
+for (let i = 0; i < year.length; i++) {
+oldCars.push(year[i]);
+}
+console.log(oldCars.length);
+
+
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
@@ -105,11 +116,35 @@ for (let i = 0; i < inventory.length; i++) {
     if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
         BMWAndAudi.push(inventory[i]);
     }
-}
+} 
+//Stretch:
+BMWAndAudi = inventory.filter(item => item.car_make === 'BMW' || item.car_make === 'Audi');
+
+
 
 console.log(JSON.stringify(BMWAndAudi));
 
 
+//stretch:
 
+const add2NumCallback = (x, y) => {
+    return x + y;
+};
 
+const checkConsistentOutputHigherOrder = (myfunc, value1, value2) => {
+    let call1 = myfunc(value1, value2);
+    let call2 = myfunc(value1, value2);
+    if (call1 === call2) {
+      return call1;
+    } else {
+      return "This function returned inconsistent results";
+    }
+  };
 
+  console.log(checkConsistentOutputHigherOrder(add2NumCallback, 1, 2));
+
+const logTheResultHigherOrder = (myfunc, value1, value2) => {
+    console.log(myfunc(value1, value2));
+};
+
+logTheResultHigherOrder(add2NumCallback, 5, 6);
